cb5825ec8570277655c815636e9b58bcb251c421
aziz şimşek <98578225+02AzizSimsek@users.noreply.github.com>
2022-01-31 19:27:12 +0300

    Add files via upload

---
+++ 5.day.py
@@ -0,0 +1,30 @@
+#Using the for loop with Python Lists
+#for Loop
+
+fruits =["Apple","Peach","Pear"]
+for fruit in fruits:
+    print(fruit)
+    
+fruits =["Apple","Peach","Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit+" pie")
+    print(fruits)    
+print(fruits) # indent position is important
+
+# -Code Exercise-
+#Average Height Exercise
+
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+print(student_heights)
+
+
+
+
+
+
+
+
+
